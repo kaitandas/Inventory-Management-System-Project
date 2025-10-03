f3e30863ba5e8411421c6cfd5ba4df940965b2f8
kaitandas <119733067+kaitandas@users.noreply.github.com>
2025-10-03 17:35:21 +0500

    Update Store.java

--- Store.java
+++ Store.java
@@ -10,7 +10,7 @@ public class Store {
     // Attributs of class Store
     public String name;
     public String Address;
-    Manager m = new Manager("abrayan", "ayan123", 3058,"ayan", "abc@gmail.com" );  // object of Manager                                                                                           
+    Manager m = new Manager("kaitandas", "kaitan1234", 3058,"kaitan", "kaitan@gmail.com" );  // object of Manager                                                                                           
     ArrayList <SalesMan> sm_list= new ArrayList<>();  // object of SalesPerson
     ArrayList <Customer> custm_list = new ArrayList<>(); // object of customer
     ArrayList <Stock> stock_list = new ArrayList<>();  // object of Stock
@@ -679,4 +679,5 @@ private class Customer {
             }
     }while(num != 0);
 }
+
 }
